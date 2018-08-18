9f5ef5ec4739b4bdd61919f275b32d7fbf9bf75a
Alexandros <35010928+Alexzg@users.noreply.github.com>
2018-08-18 09:32:51 +0300

    Add files via upload

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -3,8 +3,15 @@ $(document).ready(function() {
     // --------------------------------------------------Home page--------------
     $(".timeline-content").hide();
 
+    // Load Timeline-Content text from txt files
+    var timelineCollectElements = document.getElementsByClassName("timeline-content");
+    for (i=0; i<timelineCollectElements.length; i++) {
+        var timelineContentId = timelineCollectElements[i].id;
+        $("#"+timelineContentId).load( "/static/resources/home_page/"+timelineContentId+".txt" );
+    }
+
     // It is used to animate the "..." on the titles' name
-    $(".timeline-content-title").append('<span></span>')
+    $(".timeline-content-title").append('<span></span>');
 
     // Hidden object is used to save flags, for when a content is not hidden
     var hidden = {};
@@ -14,37 +21,35 @@ $(document).ready(function() {
         $(this).toggleClass("selected");
         // Hide or Show content
         $(this).next().slideToggle(200);
-        // textYear -> I want to change the attributes of the YearText and Graphic,
-        // so the year is used as a name of property in the "hidden" object,
-        // it is needed in order to be used as a flag,
+        // textYear -> It is needed in order to be used as an property name,
         // for when the hidden content is not displayed.
         var textYear = $(this).parent($(".timeline-block")).parent($(".timeline-row"))
         .find($(".timeline-year .text-h2"));
         // Is the <>content</> taken from inside the html.
-        var activedContentsInYear = textYear.html();
-        // The "activeTitle" property -> It is a flag because,
+        var activeContentsInYear = textYear.html();
+        // The "activeTitle" property -> It is a flag,
         // I want to check if the hidden content remains hidden;
         // so if the titles are "active" or "inactive";
         // for that I need unique property names for each contents' title;
         // the only solution was to create the name combining the "Year" the content belongs
         // and the "index" of the title.
         var activeTitle = textYear.html() + $(this).index();
-        // The "activedContentsInYear" is used to count how many timeline-content elements are not hidden;
-        // when "activedContentsInYear" = 0 all the timeline-content elements of this year are hidden
+        // The "activeContentsInYear" is used to count how many timeline-content elements are not hidden;
+        // when "activeContentsInYear" = 0 all the timeline-content elements of this year are hidden;
         // it is created dynamically and has no number in the beginning;
-        //to solve this the 'NaN' becomes '0'.
-        isNaN(event.data.value[activedContentsInYear]) ? event.data.value[activedContentsInYear] = 0 : ""
-        // Check the timeline-titles if it is actived (that means -> content is not hidden)
-        // If it is actived from before -> change its property value to "inactive";
-        // then subtract '-1' from the sum "activedContentsInYear";
+        // to solve this the 'NaN' becomes '0' during the if-statement.
+        isNaN(event.data.value[activeContentsInYear]) ? event.data.value[activeContentsInYear] = 0 : ""
+        // Check the timeline-titles if it is active (that means -> content is not hidden);
+        // If it is already active -> change its property value to "inactive";
+        // then subtract '-1' from the sum "activeContentsInYear";
         if ( event.data.value[activeTitle] === "active" ) {
-            event.data.value[activedContentsInYear] -= 1;
+            event.data.value[activeContentsInYear] -= 1;
             event.data.value[activeTitle] = "inactive";
         }
-        // If it is inactive from before -> change its property value to "active";
-        // then add '+1' to the sum "activedContentsInYear";
+        // If it is inactive -> change its property value to "active";
+        // then add '+1' to the sum "activeContentsInYear";
         else {
-            event.data.value[activedContentsInYear] += 1;
+            event.data.value[activeContentsInYear] += 1;
             event.data.value[activeTitle] = "active";
         }
 
@@ -53,7 +58,7 @@ $(document).ready(function() {
         parent = $(this).parent($(".timeline-block")).parent($(".timeline-row"))
         // If there are timeline-content elements activated, add the class "selected";
         // this means that a timeline-title in that Year has been clicked
-        if ( event.data.value[activedContentsInYear] !== 0) {
+        if ( event.data.value[activeContentsInYear] !== 0) {
             parent.find($(".timeline-year-graphic")).addClass("selected");
             parent.addClass("activated");
         }
